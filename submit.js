1bddd6d87dff60765f5fcd14ac3ab83a352d5a2f
NelsonJs <18320944165@163.com>
2021-02-01 17:38:05 +0800

    更新index

--- submit.js
+++ submit.js
@@ -1,6 +1,21 @@
-function checkForm() {
+function checkLoginForm() {
 	var user = document.getElementById('user').value;
 	var pwd = document.getElementById('pwd').value;
+	if (isEmpty(user)) {
+		alert("请填写姓名");
+		return false;
+	}
+	if (isEmpty(pwd)) {
+		alert("请输入密码");
+		return false;
+	}
+}
+
+function checkApkForm() {
+	var channel = document.forms["apk"]["channel"].value;
+	var num = document.forms["apk"]["num"].value;
+	var desc = document.forms["apk"]["desc"].value;
+	var num = document.forms["apk"]["num"].value;
 	if (isEmpty(user)) {
 		alert("请填写姓名");
 		return false;
